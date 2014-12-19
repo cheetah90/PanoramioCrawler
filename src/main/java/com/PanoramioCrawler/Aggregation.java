4aa3abb7c815e1372ea681ae577568132e206999
cheetah90 <cheetah0513@gmail.com>
2014-12-19 11:19:01 -0600

    Add query via JDBC directly

--- src/main/java/com/PanoramioCrawler/Aggregation.java
+++ src/main/java/com/PanoramioCrawler/Aggregation.java
@@ -2,10 +2,8 @@ package com.PanoramioCrawler;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.*;
+import java.util.*;
 
 import com.vividsolutions.jts.geom.Geometry;
 import org.apache.commons.csv.CSVFormat;
@@ -31,10 +29,12 @@ public class Aggregation {
     private static String dbAggreUnitTableName = null;
     private static DataStore postGISDataStore = null;
     private static String dbUGCTableName = null;
+    private static Connection dbConnection = null;
+
 
     public static void generateCSVwithHeader(){
         try {
-            FileWriter writer = new FileWriter("./out/testPhoto.csv");
+            FileWriter writer = new FileWriter("./out/output.csv");
             CSVFormat csvFormat = CSVFormat.EXCEL.withDelimiter(',');
 
             CSVPrinter csvPrinter = new CSVPrinter(writer, csvFormat);
@@ -48,17 +48,12 @@ public class Aggregation {
         }
     }
 
-    private static void addRow2Csv(String FIPS, Integer photoCounts){
-        String csvFileName = new String("./out/testPhoto.csv");
+    private static void addRow2Csv(ArrayList arrayList){
+        String csvFileName ="./out/output.csv";
         try {
             FileWriter writer = new FileWriter(csvFileName, true);
             CSVFormat csvFormat = CSVFormat.EXCEL.withDelimiter(',');
             CSVPrinter csvPrinter = new CSVPrinter(writer, csvFormat);
-
-            ArrayList<String> arrayList = new ArrayList<String>();
-            arrayList.add(FIPS);
-            arrayList.add(photoCounts.toString());
-
 
             csvPrinter.printRecord(arrayList);
 
@@ -72,6 +67,10 @@ public class Aggregation {
 
 
     private static void connect2Postgres(){
+
+        /**
+         * Connect via Geotool DataStore (Slow)
+         */
         Map<String,Object> params = new HashMap<String,Object>();
         params.put( "dbtype", "postgis");
         params.put( "host", "localhost");
@@ -86,6 +85,33 @@ public class Aggregation {
         } catch (IOException e){
             e.printStackTrace();
         }
+
+
+        /**
+         * Connect via JDBC
+         */
+
+        String dbURL = "jdbc:postgresql://localhost:5432/"+dbName;
+        Properties props = new Properties();
+        props.setProperty("user",dbUserName);
+        props.setProperty("password",dbPassword);
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+            System.out.println("Class org.postgresql.Driver has not been found!");
+            System.exit(-1);
+        }
+
+        try {
+            dbConnection = DriverManager.getConnection(dbURL, props);
+        }catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("The connection to Postgres database failed!");
+            System.exit(-2);
+        }
+
 
     }
 
@@ -109,23 +135,60 @@ public class Aggregation {
         connect2Postgres();
 
 
-        FeatureSource areaUnitsFS= null;
-        FeatureSource UgcFS = null;
+        FeatureSource areaUnitsFS;
+        FeatureSource UgcFS;
 
         try {
             areaUnitsFS = postGISDataStore.getFeatureSource(dbAggreUnitTableName);
             UgcFS = postGISDataStore.getFeatureSource(dbUGCTableName);
-            System.out.println("bc count: " + areaUnitsFS.getCount(Query.ALL));
-            System.out.println("bc count: " + UgcFS.getCount(Query.ALL));
+            System.out.println("count of area units: " + areaUnitsFS.getCount(Query.ALL));
+            System.out.println("count of ugc features: " + UgcFS.getCount(Query.ALL));
 
-            Filter pointInPolygon = null;
-            FeatureCollection photoFC = null;
-            String countyUID = null;
+            Filter pointInPolygon;
+            FeatureCollection photoFC;
+            String areaUnitUID;
+            ArrayList<String> arrayList = new ArrayList<String>();
+
             FeatureIterator countiesIterator = areaUnitsFS.getFeatures().features();
             generateCSVwithHeader();
 
+            /**
+             * Another way: directly issue query to PSQL, so we can leverage SQL aggregate function.
+             */
+            PreparedStatement getDistinctUsersCount = dbConnection.prepareStatement("SELECT COUNT(DISTINCT owner_id) FROM "+dbUGCTableName +" WHERE ST_WITHIN(geom, ST_GeomFromText(?,4326))");
+            ResultSet rsDistinctUserCount;
+            Integer count=null;
+
+            try{
+               while(countiesIterator.hasNext()){
+                   SimpleFeature countyFeature = (SimpleFeature) countiesIterator.next();
+                   Geometry countyGeometry = (Geometry) countyFeature.getDefaultGeometry();
+
+                   getDistinctUsersCount.setString(1,countyGeometry.toString());
+                   rsDistinctUserCount = getDistinctUsersCount.executeQuery();
+
+                   while (rsDistinctUserCount.next())
+                   {
+                       count = rsDistinctUserCount.getInt(1);
+                       arrayList.add(countyFeature.getAttribute("fips_str").toString());
+                       //arrayList.add(countyFeature.getAttribute("province").toString());
+                       //arrayList.add(countyFeature.getAttribute("prefecture").toString());
+                       arrayList.add(Integer.toString(count));
+                   }
+                   System.out.println("distinct users:"+ count);
+
+                   addRow2Csv(arrayList);
+                   arrayList.clear();
+
+               }
+            } finally {
+                countiesIterator.close();
+            }
+
+            /*
             try {
                 while(countiesIterator.hasNext() ){
+
                     SimpleFeature countyFeature = (SimpleFeature) countiesIterator.next();
                     Geometry countyGeometry = (Geometry) countyFeature.getDefaultGeometry();
 
@@ -133,10 +196,22 @@ public class Aggregation {
                     pointInPolygon = CQL.toFilter("WITHIN(geom,"+countyGeometry.toString()+")");
                     photoFC = UgcFS.getFeatures(pointInPolygon);
 
-                    countyUID = (String) countyFeature.getAttribute("fips_str");
+                    areaUnitUID = (String) countyFeature.getAttribute("fips_str");
+                    if (!areaUnitUID.isEmpty()){
+                        arrayList.add(areaUnitUID);
+                        //arrayList.add(countyFeature.getAttribute("province").toString());
+                        //arrayList.add(countyFeature.getAttribute("prefecture").toString());
+                        arrayList.add(Integer.toString(photoFC.size()));
 
-                    addRow2Csv(countyUID, photoFC.size());
+                        System.out.println("Current Area Unit UID is: "+areaUnitUID+"\t"+"Counts: "+Integer.toString(photoFC.size()));
 
+                        addRow2Csv(arrayList);
+                        arrayList.clear();
+                    }
+                    else {
+                        System.out.println("The UID is empty");
+                        System.exit(-1);
+                    }
                 }
             } catch (CQLException e){
                 e.printStackTrace();
@@ -144,9 +219,13 @@ public class Aggregation {
             finally {
                 countiesIterator.close();
             }
+            */
 
         } catch (IOException e){
             e.printStackTrace();
+        } catch (SQLException e){
+            e.printStackTrace();
         }
+
     }
 }
